c132ee3e82b27be2b9ec3daef3e6ce8558a581f8
Shaurya Srivastav <108766004+Shaurya-Srivastav@users.noreply.github.com>
2023-01-31 13:21:00 -0800

    added right-end point calculations

--- main.cpp
+++ main.cpp
@@ -1,51 +1,59 @@
 // Created by Shaurya Srivastav
 // Date: January 27th, 2023
-// Purpose: to calculate area underneath a function
+// Purpose: to calculate a set of outputs given a particular function
 
 #include <iostream>
 #include <math.h>
 using namespace std;
 
-
-void leftEndPointArea(int modifiedInput[], int size){
-  int width; 
-
-  cout << "Enter rectangle width: "; 
-  cin >> width;
-
-  cout << "\n";
-
-  int totalArea = 0; 
+void rightEndPointArea(int modifiedInput[], int size, int width) {
+  int totalArea = 0;
   int individualHeightOfRectangle;
-  for(int i = 0; i < size; i++){ 
-    individualHeightOfRectangle = (modifiedInput[i])*width;
+  for (int i = 1; i < size; i++) {
+    individualHeightOfRectangle = (modifiedInput[i]) * width;
     totalArea = totalArea + individualHeightOfRectangle;
   }
 
-  cout << "The total Area under the curve using the left-endpoint method is: \n" << totalArea << "\n"; 
-  
+  cout << "The total Area under the curve using the right-endpoint method is: "
+       << totalArea << "\n";
 }
 
-void functionOutput(int input[], int size) { //calculates outputs of the function and sends them to the rest
+void leftEndPointArea(int modifiedInput[], int size, int width) {
+  int totalArea = 0;
+  int individualHeightOfRectangle;
+  for (int i = 0; i < size; i++) {
+    individualHeightOfRectangle = (modifiedInput[i]) * width;
+    totalArea = totalArea + individualHeightOfRectangle;
+  }
+
+  cout << "The total Area under the curve using the left-endpoint method is: "
+       << totalArea << "\n";
+}
+
+void functionOutput(int input[], int size,
+                    int width) { // calculates outputs of the function and sends
+                                 // them to the rest
 
   int updatedDataPoints[size];
 
   for (int i = 0; i < size; i++) {
-    int updatedValues = (36 - (pow(input[i], 2))); //change this variable to reflect your function
+    int updatedValues =
+        (36 -
+         (pow(input[i], 2))); // change this variable to reflect your function
     updatedDataPoints[i] = updatedValues;
   }
 
   cout << "\nThe function has the following outputs: \n";
 
   for (int i = 0; i < size; i++) {
-     cout << updatedDataPoints[i] << "\n";
+    cout << updatedDataPoints[i] << "\n";
   }
 
   cout << "\n";
 
-  leftEndPointArea(updatedDataPoints, size); 
+  leftEndPointArea(updatedDataPoints, size, width);
+  rightEndPointArea(updatedDataPoints, size, width);
 }
-
 
 int main() {
   int numDataPoints;
@@ -59,7 +67,14 @@ int main() {
     cin >> dataValues[i];
   }
 
-  functionOutput(dataValues, numDataPoints);
+  int width;
+
+  cout << "Enter rectangle width: ";
+  cin >> width;
+
+  cout << "\n";
+
+  functionOutput(dataValues, numDataPoints, width);
 
   return 0;
 }
