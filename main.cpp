d0a264b84ec51b2386f2dc3615ee1de9ca81e527
Shaurya Srivastav <108766004+Shaurya-Srivastav@users.noreply.github.com>
2023-09-13 22:12:00 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,70 +1,72 @@
 // Created by Shaurya Srivastav
-// Date: January 27th, 2023
-// Purpose: to calculate a set of outputs given a particular function
+// Date: September 13, 2023
+// Purpose: Calculate integrals and graph mathematical functions
 
 #include <iostream>
-#include <math.h>
-using namespace std;
+#include <cmath>
+#include <functional>
+#include <vector>
+#include <string>
+#include <sstream>
+#include "matplotlibcpp.h" // Requires matplotlib-cpp for plotting (https://github.com/lava/matplotlib-cpp)
 
-void rightEndPointArea(int modifiedInput[], int size, int width) {
-  int totalArea = 0;
-  int individualHeightOfRectangle;
+namespace plt = matplotlibcpp;
+
+const int kMaxDataPoints = 100;
+
+int RightEndPointArea(const int modified_input[], int size, int width) {
+  int total_area = 0;
   for (int i = 1; i < size; i++) {
-    individualHeightOfRectangle = (modifiedInput[i]) * width;
-    totalArea = totalArea + individualHeightOfRectangle;
+    int individual_height = modified_input[i] * width;
+    total_area += individual_height;
   }
-
-  cout << "The total Area under the curve using the right-endpoint method is: "
-       << totalArea << "\n";
+  return total_area;
 }
 
-void leftEndPointArea(int modifiedInput[], int size, int width) {
-  int totalArea = 0;
-  int individualHeightOfRectangle;
+int LeftEndPointArea(const int modified_input[], int size, int width) {
+  int total_area = 0;
   for (int i = 0; i < size; i++) {
-    individualHeightOfRectangle = (modifiedInput[i]) * width;
-    totalArea = totalArea + individualHeightOfRectangle;
+    int individual_height = modified_input[i] * width;
+    total_area += individual_height;
   }
-
-  cout << "The total Area under the curve using the left-endpoint method is: "
-       << totalArea << "\n";
+  return total_area;
 }
 
-void functionOutput(int input[], int size,
-                    int width) { // calculates outputs of the function and sends
-                                 // them to the rest
+// Function to calculate the integral using trapezoidal rule
+double TrapezoidalIntegral(std::function<double(double)> func, double a, double b, int num_intervals) {
+  double h = (b - a) / num_intervals;
+  double integral = 0.5 * (func(a) + func(b));
 
-  int updatedDataPoints[size];
-
-  for (int i = 0; i < size; i++) {
-    int updatedValues =
-        (36 -
-         (pow(input[i], 2))); // change this variable to reflect your function
-    updatedDataPoints[i] = updatedValues;
+  for (int i = 1; i < num_intervals; i++) {
+    double x = a + i * h;
+    integral += func(x);
   }
 
-  cout << "\nThe function has the following outputs: \n";
+  return h * integral;
+}
 
-  for (int i = 0; i < size; i++) {
-    cout << updatedDataPoints[i] << "\n";
-  }
-
-  cout << "\n";
-
-  leftEndPointArea(updatedDataPoints, size, width);
-  rightEndPointArea(updatedDataPoints, size, width);
+// Function to parse a mathematical expression and return a lambda function
+std::function<double(double)> ParseExpression(const std::string& expression) {
+  std::string parsed_expression = "[](double x) { return " + expression + "; }";
+  std::function<double(double)> func = std::function<double(double)>(eval(parsed_expression));
+  return func;
 }
 
 int main() {
-  int numDataPoints;
-  cout << "Enter the amount of x-values you need: ";
-  cin >> numDataPoints;
+  int num_data_points;
+  cout << "Enter the number of x-values you need: ";
+  cin >> num_data_points;
 
-  int dataValues[numDataPoints];
+  if (num_data_points > kMaxDataPoints) {
+    cout << "Error: Number of data points exceeds the maximum allowed." << endl;
+    return 1;
+  }
 
-  for (int i = 0; i < numDataPoints; i++) {
+  int data_values[kMaxDataPoints];
+
+  for (int i = 0; i < num_data_points; i++) {
     cout << "Input data value " << (i + 1) << ": ";
-    cin >> dataValues[i];
+    cin >> data_values[i];
   }
 
   int width;
@@ -74,7 +76,45 @@ int main() {
 
   cout << "\n";
 
-  functionOutput(dataValues, numDataPoints, width);
+  int left_area = LeftEndPointArea(data_values, num_data_points, width);
+  int right_area = RightEndPointArea(data_values, num_data_points, width);
+
+  cout << "The total Area under the curve using the left-endpoint method is: "
+       << left_area << "\n";
+  cout << "The total Area under the curve using the right-endpoint method is: "
+       << right_area << "\n";
+
+  // Input and integrate a function
+  double a, b;
+  int num_intervals;
+  std::string user_expression;
+
+  cout << "Enter the integration interval [a, b]: ";
+  cin >> a >> b;
+  cout << "Enter the number of intervals for integration: ";
+  cin >> num_intervals;
+
+  cout << "Enter a mathematical expression (e.g., 'x^2 + sin(x)'): ";
+  cin.ignore();
+  std::getline(cin, user_expression);
+
+  // Parse the user's mathematical expression
+  auto func = ParseExpression(user_expression);
+
+  double integral = TrapezoidalIntegral(func, a, b, num_intervals);
+  cout << "The integral of the function within the interval [" << a << ", " << b << "] is: " << integral << "\n";
+
+  // Plot the user's function
+  std::vector<double> x_values, y_values;
+  double step = (b - a) / 100; // 100 points for the graph
+  for (double x = a; x <= b; x += step) {
+    x_values.push_back(x);
+    y_values.push_back(func(x));
+  }
+
+  plt::plot(x_values, y_values);
+  plt::title("Graph of the Function");
+  plt::show();
 
   return 0;
 }
